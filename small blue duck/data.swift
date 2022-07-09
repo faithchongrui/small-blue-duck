b8389ce60b511e0715a9fc79cc4fa8f9539dd167
faith <89432125+faithchongrui@users.noreply.github.com>
2022-07-09 10:57:01 +0800

    made a friend struct

---
+++ small blue duck/data.swift
@@ -0,0 +1,14 @@
+//
+//  data.swift
+//  small blue duck
+//
+//  Created by FAITH CHONG RUI EN stu on 9/7/22.
+//
+
+import Foundation
+
+struct Friend {
+    var name: String
+    var stats: Int
+    var school: String
+}
