a5bab48ba91a59fef8e6ab11bda7ff8af86aa260
faith <89432125+faithchongrui@users.noreply.github.com>
2022-07-09 10:54:08 +0800

    Initial Commit

---
+++ small blue duck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  small blue duck
+//
+//  Created by FAITH CHONG RUI EN stu on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ small blue duck/small_blue_duckApp.swift
@@ -0,0 +1,17 @@
+//
+//  small_blue_duckApp.swift
+//  small blue duck
+//
+//  Created by FAITH CHONG RUI EN stu on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct small_blue_duckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
